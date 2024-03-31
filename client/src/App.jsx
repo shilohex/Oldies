9829d3adee38ce666826cf62d60e15de94b79e10
Excel oladipupo <shiloholadipupo@gamil.com>
2024-03-31 22:32:40 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import React from "react";
 import Signup from "./Pages/signup/Signup";
+import Login from "./Pages/login/Login.";
 import Landingpage from "./Pages/landingpage/Landingpage";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landingpage />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="./login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
