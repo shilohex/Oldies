4b634b2752b4fc79d98e00edf51dfd486759cfb5
Excel oladipupo <shiloholadipupo@gamil.com>
2024-05-02 14:36:02 +0100

    edits

--- client/src/Pages/auth/login/Login.jsx
+++ client/src/Pages/auth/login/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://oldies.onrender.com//user/login",
+        "https://oldies.onrender.com/user/login",
         {
           email: email,
           password: password,
