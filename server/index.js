f17a21af217dc97258f492b20af2bca19fdc3d74
Excel oladipupo <shiloholadipupo@gamil.com>
2024-05-14 13:06:35 +0100

    new edit

--- server/index.js
+++ server/index.js
@@ -8,23 +8,41 @@ const userRoutes = require("./routes/userRoutes");
 const VendorRoutes = require("./routes/vendorRoutes");
 const ProductRoutes = require("./routes/productRoutes");
 dotenv.config();
-connectdb();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://oldies-murex.vercel.app",
-    ],
-  })
-);
+const allowedOrigins = [
+  "https://oldies-murex.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:5174",
+];
+// middleware
+app.use(function (req, res, next) {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, authorization"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
 
 app.use("/user", userRoutes);
 app.use("/vendor", VendorRoutes);
 app.use("/product", ProductRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(port, async () => {
+  try {
+    await connectdb();
+    console.log(`Server running on port ${port}`);
+  } catch (error) {
+    console.log(error);
+  }
 });
